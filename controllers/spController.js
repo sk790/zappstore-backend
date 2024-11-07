c2ebe95872bcb95c760284727471979fbfd42ee1
sk790 <saurabhkumar16333@gmail.com>
2024-11-07 13:59:00 +0530

    update sp controller

--- controllers/spController.js
+++ controllers/spController.js
@@ -1,18 +1,16 @@
 import Service from "../models/serviceModel.js";
 
 export const getSp = async (req, res) => {
-  const { service, coords } = req.body;
+  const { service } = req.body;
   try {
-    const nearbyProviders = await Service.find({
-      location: {
-        $geoWithin: {
-          $centerSphere: [[coords.longitude, coords.latitude], 10 / 6378.1], // Radius in radians (10 km / Earth's radius)
-        },
-      },
-      serviceType: service,
-    });
-    return res.status(200).json({ sp: nearbyProviders });
+    const sp = await Service.find({ serviceType: service }).populate(
+      "provider"
+    );
+    if (!sp) {
+      return res.status(404).json({ message: "Service not found" });
+    }
+    res.status(200).json({ message: "SP fetched successfully", sp });
   } catch (error) {
-    return res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message });
   }
 };
